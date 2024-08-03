9db98a5ef05a9722d8e7502d7e91bc2a0b6b76a6
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-08-03 17:28:27 -0300

    feat: creating card loading logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,36 @@
-let cardNumber = 1;
-const card = `
-<div class="card">
-   <h2>Buuh!</h2>
-</div>`;
-const cardTurned = `<div class="card turned">
-   <img src="./assets/card0${cardNumber}.svg"/>
-</div>`;
+let cardNumber;
+let cardsList = [];
+const cards_container = document.querySelector('.cards-container');
 
-const cards = document.querySelector('.cards-container');
-
-for (let i = 0; i < 18; i++) {
-   cardNumber = i;
-   cards.innerHTML += card;
+while (cardsList.length <= 17) {
+   cardNumber = Number(Math.random() * (9 - 1) + 1).toFixed();
+   const isCardInList =
+      cardsList.filter((card) => card === cardNumber).length < 2;
+   if (isCardInList) {
+      console.log(cardsList.filter((card) => card === cardNumber).length);
+      cardsList.push(cardNumber);
+   }
 }
+
+for (let i = 0; i < cardsList.length; i++) {
+   let card = `
+   <div class="card">
+      <h2>Buuh!</h2>
+      <img class="hidden" src="./assets/card0${cardsList[i]}.svg"/>
+   </div>`;
+   cards_container.innerHTML += card;
+}
+const cards = document.querySelectorAll('.card');
+
+function cardHandler(card) {
+   const cardTitle = card.querySelector('h2');
+   const cardImg = card.querySelector('img');
+
+   card.classList.toggle('turned');
+   cardTitle.classList.toggle('hidden');
+   cardImg.classList.toggle('hidden');
+}
+
+cards.forEach((cardItem) => {
+   cardItem.addEventListener('click', () => cardHandler(cardItem));
+});
