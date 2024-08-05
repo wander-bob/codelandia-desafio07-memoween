624e36ee25b4df15f405cf8fe536d7c8db3c9cff
Wanderson Alves do Nascimento <wanderson.alves.n@outlook.com>
2024-08-05 09:26:18 -0300

    feat: updating JS to handle turned cards and register score to define a winner

--- js/main.js
+++ js/main.js
@@ -1,36 +1,66 @@
-let cardNumber;
+let cardNumber = 0;
 let cardsList = [];
+let turnedCards = [];
+let successCount = 0;
+
 const cards_container = document.querySelector('.cards-container');
 
 while (cardsList.length <= 17) {
-   cardNumber = Number(Math.random() * (9 - 1) + 1).toFixed();
-   const isCardInList =
-      cardsList.filter((card) => card === cardNumber).length < 2;
-   if (isCardInList) {
-      console.log(cardsList.filter((card) => card === cardNumber).length);
-      cardsList.push(cardNumber);
-   }
+  cardNumber = Number(Math.random() * (9 - 1) + 1).toFixed();
+  const isCardInList =
+    cardsList.filter((card) => card === cardNumber).length < 2;
+  if (isCardInList) {
+    cardsList.push(cardNumber);
+  }
 }
 
 for (let i = 0; i < cardsList.length; i++) {
-   let card = `
+  let card = `
    <div class="card">
       <h2>Buuh!</h2>
       <img class="hidden" src="./assets/card0${cardsList[i]}.svg"/>
    </div>`;
-   cards_container.innerHTML += card;
+  cards_container.innerHTML += card;
 }
+
 const cards = document.querySelectorAll('.card');
 
-function cardHandler(card) {
-   const cardTitle = card.querySelector('h2');
-   const cardImg = card.querySelector('img');
+function cardTurnHandler(card) {
+  const cardTitle = card.querySelector('h2');
+  const cardImg = card.querySelector('img');
 
-   card.classList.toggle('turned');
-   cardTitle.classList.toggle('hidden');
-   cardImg.classList.toggle('hidden');
+  card.classList.toggle('turned');
+  cardTitle.classList.toggle('hidden');
+  cardImg.classList.toggle('hidden');
+  turnedCards.push(card);
+}
+
+function handleInteractionCount() {
+  if (turnedCards.length === 2) {
+    const [card1, card2] = turnedCards;
+    if (card1.querySelector('img').src === card2.querySelector('img').src) {
+      console.log('done!');
+      successCount++;
+      turnedCards = [];
+      return;
+    }
+    setTimeout(() => {
+      console.log('wrong');
+      turnedCards.forEach((card) => {
+        cardTurnHandler(card);
+      });
+      turnedCards = [];
+      return;
+    }, 500);
+  }
+  if (successCount === 9) {
+    console.log('You win!!!');
+  }
 }
 
 cards.forEach((cardItem) => {
-   cardItem.addEventListener('click', () => cardHandler(cardItem));
+  cardItem.addEventListener('click', () => {
+    cardTurnHandler(cardItem);
+    handleInteractionCount();
+  });
 });
